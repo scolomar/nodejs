45b6d80b4795c48c2e36ab86b266d194d3d33ff4
secobau <sebastian.colomar@gmail.com>
2019-09-10 20:33:33 +0200

    Update index.js

--- rest-api/index.js
+++ rest-api/index.js
@@ -4,16 +4,16 @@ let store = {}
 store.accounts = []
 let app = express()
 app.use(bodyParser.json())
-app.get('/accounts',(request,response)=>{response.status(200).send(store.accounts)})
-app.post('/accounts',(request,response)=>{let newAccount=request.body;
+app.get('/API',(request,response)=>{response.status(200).send(store.accounts)})
+app.post('/API',(request,response)=>{let newAccount=request.body;
 	let idAccount=store.accounts.length;
 	store.accounts.push(newAccount);
 	response.status(201).send({id:idAccount});
 })
-app.put('/accounts/:a',(x,y)=>{store.accounts[x.params.a]=x.body;
+app.put('/API/:a',(x,y)=>{store.accounts[x.params.a]=x.body;
 	y.status(200).send(store.accounts[x.params.a]);
 })
-app.delete('/accounts/:a',(x,y)=>{store.accounts.splice(x.params.a,1);
+app.delete('/API/:a',(x,y)=>{store.accounts.splice(x.params.a,1);
 	y.status(204).send();
 })
 app.listen(8080)
